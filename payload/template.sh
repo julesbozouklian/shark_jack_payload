344ec6920dfaf0018f220d17f9c4c9c413d861dd
Jules Bozouklian <jules.bozouklian@epitech.eu>
2022-01-17 21:59:28 +0100

    Update template.sh

--- payload/template.sh
+++ payload/template.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
-#
-# Title:         Jack Tester
+#!/bin/bh
+# Title:         Template
+# Description:   Description
 # Author:        Jules Bozouklian - bozou_client
 # Version:       1.0
+# Category:      Template
 #
-# Description
-#
-# Magenta Solid............Setup
-# Red Slow Blink...........Inactive Jack
-# Green....................Active Jack
+# LED SETUP (Magenta)... Setting logs and waiting for IP address from DHCP
+# LED ATTACK (Yellow Blink)... Saving IP address information
+# LED FAIL (Red Blink)... Failed to gather public IP address
+# LED SPECIAL (Cyan Blink)... Exfiltrating log to Cloud C2
+# LED FINISH (Green Fast Blink to Solid)... Payload successful
 #
