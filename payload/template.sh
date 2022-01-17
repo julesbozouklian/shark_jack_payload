5e9dd6ebaa680cf557c025399b962d7805f8282d
Jules Bozouklian <jules.bozouklian@epitech.eu>
2022-01-17 21:41:37 +0100

    Update and rename 0_tests.sh to template.sh

---
+++ payload/template.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Title:         Jack Tester
+# Author:        Jules Bozouklian - bozou_client
+# Version:       1.0
+#
+# Description
+#
+# Magenta Solid............Setup
+# Red Slow Blink...........Inactive Jack
+# Green....................Active Jack
+#
